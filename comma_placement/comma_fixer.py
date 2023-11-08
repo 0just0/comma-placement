70f9aeaead057876f4ebd1afac2e9a694f25e39a
Dmitry Temnov <d.temnov@sdgroup.ai>
2023-11-08 17:31:48 -0500

    Precommit fixes

---
+++ comma_placement/comma_fixer.py
@@ -0,0 +1,74 @@
+import torch
+from config import ID2LABEL, LABEL2ID
+from peft import PeftConfig, PeftModel
+from transformers import AutoModelForTokenClassification, AutoTokenizer
+import warnings
+
+
+class CommaFixer:
+    def __init__(self, config_path: str, device: str) -> None:
+        self.config_path = config_path
+        self.device = device
+        model, tokenizer = self.prepare_model(self.config_path, self.device)
+        self.model = model
+        self.tokenizer = tokenizer
+
+    def prepare_model(self, config_path: str, device: str):
+        config = PeftConfig.from_pretrained(config_path)
+        inference_model = AutoModelForTokenClassification.from_pretrained(
+            config.base_model_name_or_path,
+            num_labels=2,
+            id2label=ID2LABEL,
+            label2id=LABEL2ID,
+        )
+        tokenizer = AutoTokenizer.from_pretrained(
+            config.base_model_name_or_path, add_prefix_space=True
+        )
+        model = PeftModel.from_pretrained(inference_model, config_path)
+        model.to(device)
+        model.eval()
+        return model, tokenizer
+
+    def __infer(self, text):
+        tokenized = self.tokenizer(
+            text, return_tensors="pt", return_offsets_mapping=True, return_length=True
+        )
+        tokenized.to(self.model.device)
+        with torch.inference_mode():
+            logits = self.model(
+                tokenized["input_ids"], tokenized["attention_mask"]
+            ).logits
+        tokens = tokenized.tokens()
+        predictions = torch.argmax(logits, dim=2).detach().cpu()
+        labels = [
+            self.model.config.id2label[prediction]
+            for prediction in predictions[0].numpy()
+        ]
+        return tokens, labels, tokenized["offset_mapping"][0].detach().cpu().numpy()
+
+    def __fix_commas_based_on_labels_and_offsets(
+        self, labels: list[str], original_text: str, offset_map: list[tuple[int, int]]
+    ) -> str:
+        result = original_text
+        commas_inserted = 0
+
+        for i, label in enumerate(labels):
+            current_offset = offset_map[i][1] + commas_inserted
+            if label == "B-COMMA":
+                result = result[:current_offset] + "," + result[current_offset:]
+                commas_inserted += 1
+        return result
+
+    def fix_commas(self, text: str) -> str:
+        _, predictions, offset = self.__infer(text)
+        res = self.__fix_commas_based_on_labels_and_offsets(predictions, text, offset)
+        return res
+
+
+if __name__ == "__main__":
+    warnings.warn("This module shouldn't be called directy.")
+    comma_fixer = CommaFixer(
+        "just097/roberta-base-lora-comma-placement-r-8-alpha-32", "cpu"
+    )
+    res = comma_fixer.fix_commas("One two three.")
+    print(f"Formatted string with commas: {res}")
--- comma_placement/inference.py
+++ comma_placement/inference.py
@@ -1,9 +1,6 @@
-import torch
-from config import ID2LABEL, LABEL2ID
-from peft import PeftConfig, PeftModel
-from transformers import AutoModelForTokenClassification, AutoTokenizer
-
 import argparse
+
+from comma_fixer import CommaFixer
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -12,63 +9,18 @@ parser.add_argument(
     default="just097/roberta-base-lora-comma-placement-finetuned",
     help="Please provide a model-id on HF",
 )
-parser.add_argument("--input", type=str, default="One two three.", help="Enter text without commas.")
+parser.add_argument(
+    "--input", type=str, default="One two three.", help="Enter text without commas."
+)
 parser.add_argument("--device", default="cpu")
 args = parser.parse_args()
 
 peft_model_id = args.model
-model = None
-tokenizer = None
 device = args.device
-
-
-def prepare_model(config_path: str, device: str):
-    config = PeftConfig.from_pretrained(peft_model_id)
-    inference_model = AutoModelForTokenClassification.from_pretrained(
-        config.base_model_name_or_path,
-        num_labels=2,
-        id2label=ID2LABEL,
-        label2id=LABEL2ID,
-    )
-    tokenizer = AutoTokenizer.from_pretrained(config.base_model_name_or_path, add_prefix_space=True)
-    model = PeftModel.from_pretrained(inference_model, peft_model_id)
-    model.to(device)
-    model.eval()
-    return model, tokenizer
-
-
-def infer(text):
-    tokenized = tokenizer(text, return_tensors="pt", return_offsets_mapping=True, return_length=True)
-    tokenized.to(model.device)
-    with torch.inference_mode():
-        logits = model(tokenized["input_ids"], tokenized["attention_mask"]).logits
-    tokens = tokenized.tokens()
-    predictions = torch.argmax(logits, dim=2).detach().cpu()
-    labels = [model.config.id2label[prediction] for prediction in predictions[0].numpy()]
-    return tokens, labels, tokenized["offset_mapping"][0].detach().cpu().numpy()
-
-
-def fix_commas_based_on_labels_and_offsets(
-    labels: list[str], original_text: str, offset_map: list[tuple[int, int]]
-) -> str:
-    result = original_text
-    commas_inserted = 0
-
-    for i, label in enumerate(labels):
-        current_offset = offset_map[i][1] + commas_inserted
-        if label == "B-COMMA":
-            result = result[:current_offset] + "," + result[current_offset:]
-            commas_inserted += 1
-    return result
-
-
-def convert_to_text(text: str) -> str:
-    _, predictions, offset = infer(text)
-    res = fix_commas_based_on_labels_and_offsets(predictions, text, offset)
-    return res
 
 
 if __name__ == "__main__":
     sample_sentence = args.input
-    model, tokenizer = prepare_model(peft_model_id, args.device)
-    print(convert_to_text(sample_sentence))
+    comma_fixer = CommaFixer(peft_model_id, device)
+    res = comma_fixer.fix_commas(sample_sentence)
+    print(f"Formatted string with commas:\n {res}")
