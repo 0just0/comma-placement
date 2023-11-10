c6e6bcebf2f47ab1dcb07ce5f766cf700705d5f3
Dmitry Temnov <d.temnov@sdgroup.ai>
2023-11-10 06:43:23 -0500

    Support longer texts

--- comma_placement/comma_fixer.py
+++ comma_placement/comma_fixer.py
@@ -3,6 +3,9 @@ from .config import ID2LABEL, LABEL2ID
 from peft import PeftConfig, PeftModel
 from transformers import AutoModelForTokenClassification, AutoTokenizer
 import warnings
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
 
 
 class CommaFixer:
@@ -32,8 +35,15 @@ class CommaFixer:
         text = text.replace(",", "")
         return text
 
+    def __split_by_sentence(self, text) -> list:
+        doc = nlp(text)
+        sentences = [str(sent) for sent in doc.sents]
+        return sentences
+
     def __infer(self, text):
-        tokenized = self.tokenizer(text, return_tensors="pt", return_offsets_mapping=True, return_length=True)
+        tokenized = self.tokenizer(
+            text, return_tensors="pt", padding=True, return_offsets_mapping=True, return_length=True
+        )
         tokenized.to(self.model.device)
         with torch.inference_mode():
             logits = self.model(tokenized["input_ids"], tokenized["attention_mask"]).logits
@@ -50,20 +60,31 @@ class CommaFixer:
 
         for i, label in enumerate(labels):
             current_offset = offset_map[i][1] + commas_inserted
-            if label == "B-COMMA":
+            if label == "B-COMMA" and result[current_offset].isspace():
                 result = result[:current_offset] + "," + result[current_offset:]
                 commas_inserted += 1
         return result
 
     def fix_commas(self, text: str) -> str:
+        result = []
         text = self.remove_commas(text)
-        _, predictions, offset = self.__infer(text)
-        res = self.__fix_commas_based_on_labels_and_offsets(predictions, text, offset)
-        return res
+        if len(text) > 512:
+            text = self.__split_by_sentence(text)
+        else:
+            text = [text]
+        for t in text:
+            _, predictions, offset = self.__infer(t)
+            res = self.__fix_commas_based_on_labels_and_offsets(predictions, t, offset)
+            result.append(res)
+
+        return " ".join(result)
 
 
 if __name__ == "__main__":
     warnings.warn("This module shouldn't be called directy.")
     comma_fixer = CommaFixer("just097/roberta-base-lora-comma-placement-r-8-alpha-32", "cpu")
-    res = comma_fixer.fix_commas("One two three.")
+    test_input = """In a quaint, little town nestled between rolling hills and meandering rivers, there lived a diverse community of individuals, each with their own dreams, aspirations, and stories to tell. The townspeople, with their friendly smiles and warm greetings, created a welcoming atmosphere that embraced newcomers and made them feel like they belonged.
+As the seasons changed, the town transformed, adorned with the vibrant colors of spring flowers, the golden hues of summer sunsets, the fiery reds and oranges of autumn leaves, and the glistening white blanket of winter snow. Throughout the year, community events brought everyone together, from lively summer festivals that filled the air with music and laughter to cozy winter gatherings where the aroma of hot cocoa and freshly baked cookies wafted through the air. The heart of the town was its central square, a bustling hub where locals gathered to chat, share stories, and celebrate life's simple pleasures. The square was adorned with a majestic fountain, its cascading water providing a soothing melody that accompanied the joyful chatter of the townspeople.
+In the heart of the square stood a centuries-old oak tree, its sturdy branches stretching out like open arms, offering shade to those seeking refuge from the summer sun. Underneath its leafy canopy, friends gathered for picnics, children played games, and elders shared the wisdom of years gone by."""
+    res = comma_fixer.fix_commas(test_input)
     print(f"Formatted string with commas: {res}")
--- tests/test_model_check.py
+++ tests/test_model_check.py
@@ -8,24 +8,27 @@ def model():
 
 
 def test_invariance(model):
-    samples = ["One two three.", "Four five six."]
+    samples = ["One, two, three.", "Four, five, six."]
     predicted = [model.fix_commas(sample) for sample in samples]
-    assert predicted == ["One, two, three.", "Four, five, six."]
+    assert predicted == samples
 
 
 def test_directional(model):
-    samples = ["Coffee an apple a milk", "There are no commas here.", "However there is a comma here."]
+    samples = ["Coffee, an apple, a milk.", "There are no commas here.", "However, there is a comma here."]
     predicted = [model.fix_commas(sample) for sample in samples]
-    assert predicted == ["Coffee, an apple, a milk,", "There are no commas here.", "However, there is a comma here."]
+    assert predicted == samples
 
 
 def test_long_sentence(model):
-    sample = """As the sun dipped below the horizon painting the sky in shades of orange red and purple the tranquility of the evening was interrupted by the sudden appearance of a shooting star."""
+    sample = "As the sun dipped below the horizon, painting the sky in shades of orange, red and purple, the tranquility of the evening was interrupted by the sudden appearance of a shooting star."
     predicted = model.fix_commas(sample)
-    assert (
-        predicted
-        == "As the sun dipped below the horizon, painting the sky in shades of orange, red and purple, the tranquility of the evening was interrupted by the sudden appearance of a shooting star."
-    )
+    assert predicted == sample
+
+
+def test_multiple_sentences(model):
+    sample = """You have probably also heard a lot of tips on using commas in addition to these rules: “Use one wherever you would naturally use a pause” or “Read your work aloud, and whenever you feel yourself pausing, put in a comma.” These techniques help to a degree, but our ears tend to trick us and we need other avenues of attack."""
+    predicted = model.fix_commas(sample)
+    assert predicted == sample
 
 
 def test_fix_wrong(model):
